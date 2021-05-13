d025905f6b290337b7aa054f24b1cf1ed753f7ba
IlyaNesterov <Eildos95@gmail.com>
2021-05-13 17:09:19 +0300

    Shuffle the Array

---
+++ shuffle-the-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+        for i in range(len(nums) // 2):
+            res.append(nums[i])
+            res.append(nums[(len(nums) // 2)+i])
+        return res
