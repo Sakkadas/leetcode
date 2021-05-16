7449d2c0a09b813ee02e71f873dfaaf9c7f7fe98
IlyaNesterov <Eildos95@gmail.com>
2021-05-16 18:51:53 +0300

    Update docs

--- Easy tasks/check-if-the-sentence-is-pangram.py
+++ Easy tasks/check-if-the-sentence-is-pangram.py
@@ -1,9 +1,31 @@
+"""
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram,
+ or false otherwise.
+
+Example 1:
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains at least one of every letter of the English alphabet.
+
+Example 2:
+Input: sentence = "leetcode"
+Output: false
+
+ """
+
 from string import ascii_lowercase,ascii_uppercase
 
 
 class Solution:
-    def checkIfPangram(self, sentence: str) -> bool:
+    def checkIfPangram(self, sentence):
         if set(sentence) == set(ascii_lowercase) or set(sentence) == set(ascii_uppercase):
             return True
         else:
             return False
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.checkIfPangram("thequickbrownfoxjumpsoverthelazydog"))
--- Easy tasks/defanging-an-ip-address.py
+++ Easy tasks/defanging-an-ip-address.py
@@ -1,4 +1,24 @@
+"""
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+Example 1:
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+
+Example 2:
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+"""
+
+
 class Solution:
-    def defangIPaddr(self, address: str) -> str:
+    def defangIPaddr(self, address):
         address = address.replace('.', '[.]')
         return address
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.defangIPaddr("1.1.1.1"))
