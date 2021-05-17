0a7f707678607349f8b1effe56e982df1fcb1933
IlyaNesterov <Eildos95@gmail.com>
2021-05-17 18:52:34 +0300

    Top K Frequent Elements

---
+++ Medium tasks/top-k-frequent-elements.py
@@ -0,0 +1,24 @@
+"""
+Given an integer array nums and an integer k, return the k most frequent elements.
+You may return the answer in any order.
+
+Example 1:
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+"""
+
+from collections import Counter
+
+
+class Solution:
+    def topKFrequent(self, nums, k):
+        return [i[0] for i in Counter(nums).most_common(k)]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.topKFrequent(nums=[1, 1, 1, 2, 2, 3], k=2))
